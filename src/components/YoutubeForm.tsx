e65829a0cd7341a4ff5361f56ef58e6e1d6d67da
nzkks <kkshanthosh@gmail.com>
2024-04-04 15:09:20 +1300

    Add form validation and give dev tool feedback

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -23,15 +23,34 @@ const YoutubeForm = () => {
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
       {/* why dividing by 2 because in development mode, <React.StrictMode /> rerenders the app/component twice and also re-run the Effects twice to find bugs - https://react.dev/reference/react/StrictMode */}
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        {/* specifying noValidate prevent browser validation and allow React hook form to takeover the validation */}
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register('username')} />
+        <input
+          type="text"
+          id="username"
+          {...register('username', {
+            required: {
+              value: true,
+              message: 'Username is required'
+            }
+          })}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" {...register('email')} />
+        <input
+          type="text"
+          id="email"
+          {...register('email', {
+            pattern: {
+              value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: 'Invalid email format'
+            }
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register('channel')} />
+        <input type="text" id="channel" {...register('channel', { required: 'Channel is required' })} />
 
         <button>Submit</button>
       </form>
