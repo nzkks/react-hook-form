55997b029f84f530f983465840b2ced5bfb1bbd0
nzkks <kkshanthosh@gmail.com>
2024-04-04 13:14:00 +1300

    Connect the useForm hook to the form elements

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,15 +1,20 @@
+import { useForm } from 'react-hook-form';
+
 const YoutubeForm = () => {
+  const form = useForm();
+  const { register } = form;
+
   return (
     <div>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register('username')} />
 
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" />
+        <input type="text" id="email" {...register('email')} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register('channel')} />
 
         <button>Submit</button>
       </form>
