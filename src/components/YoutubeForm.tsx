410e5b1980495ed63d13f663dc1084f30285ee85
nzkks <kkshanthosh@gmail.com>
2024-04-04 16:10:32 +1300

    Another way (now validate is an object) of adding the same validation rule

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -50,8 +50,10 @@ const YoutubeForm = () => {
                 value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                 message: 'Invalid email format'
               },
-              validate: fieldValue => {
-                return fieldValue !== 'admin@example.com' || 'Enter a different email address';
+              validate: {
+                notAdmin: fieldValue => {
+                  return fieldValue !== 'admin@example.com' || 'Enter a different email address';
+                }
               }
             })}
           />
