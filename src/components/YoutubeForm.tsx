40f8f6a28712f0538a48f94696ed327202f6f53c
nzkks <kkshanthosh@gmail.com>
2024-04-04 14:43:06 +1300

    Proof of React Hook Form doesn't rerender the component on every key stroke happens in its form fields.

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,12 +1,18 @@
 import { useForm } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
+let renderCount = 0;
+
 const YoutubeForm = () => {
   const form = useForm();
   const { register, control } = form;
 
+  renderCount++;
+
   return (
     <div>
+      <h1>YouTube Form ({renderCount / 2})</h1>
+      {/* why dividing by 2 because in development mode, <React.StrictMode /> rerenders the app/component twice and also re-run the Effects twice to find bugs - https://react.dev/reference/react/StrictMode */}
       <form>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register('username')} />
