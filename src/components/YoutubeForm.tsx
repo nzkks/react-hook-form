763d0e8aa4fa833d635a850d99ce2ffccb413014
nzkks <kkshanthosh@gmail.com>
2024-04-04 14:49:08 +1300

    Form Submission

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -3,9 +3,19 @@ import { DevTool } from '@hookform/devtools';
 
 let renderCount = 0;
 
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 const YoutubeForm = () => {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmit = (data: FormValues) => {
+    console.log('Form submitted', data);
+  };
 
   renderCount++;
 
@@ -13,7 +23,7 @@ const YoutubeForm = () => {
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
       {/* why dividing by 2 because in development mode, <React.StrictMode /> rerenders the app/component twice and also re-run the Effects twice to find bugs - https://react.dev/reference/react/StrictMode */}
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register('username')} />
 
